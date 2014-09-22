5895cb34c686b0db5e142abd3e77ce56650026af
Fanakartal - METU <fanakartal@gmail.com>
2014-09-22 12:08:37 +0300

    kaleci hareketi yapıldı.
    kaleci sağa sola hareket ediyor. kale, top içeri girecek şekilde
    düzenlendi.

--- Assets/Scripts/KeeperController.cs
+++ Assets/Scripts/KeeperController.cs
@@ -3,33 +3,40 @@ using System.Collections;
 
 public class KeeperController : MonoBehaviour {
 
-    float speed = 16.0f;
+    float speed = 2.0f;
     public float xBoundry;
     
     // Use this for initialization
 	void Start () 
     {
-        rigidbody2D.AddForce(new Vector2(speed, 0));
+        rigidbody2D.velocity = new Vector2(speed, 0);
+        //rigidbody2D.AddForce(new Vector2(speed, 0));
         //transform.position = new Vector3((transform.position.x + speed * Time.deltaTime), transform.position.y, transform.position.z);
 	}
 	
 	// Update is called once per frame
-	void Update () 
+	void FixedUpdate () 
     {
-
+        //Debug.Log(rigidbody2D.velocity.magnitude);
 
         if (transform.position.x >= xBoundry)
         {
             Debug.Log("X'ten büyük");
             //transform.position = new Vector3(xBoundry, transform.position.y, transform.position.z);
-            transform.position = new Vector3((transform.position.x - speed * Time.deltaTime), transform.position.y, transform.position.z);
+            //transform.position = new Vector3(-transform.position.x, transform.position.y, transform.position.z);
+            //rigidbody2D.AddForce(new Vector2(-speed, 0));
+            rigidbody2D.velocity = -(rigidbody2D.velocity);
+            //Debug.Log(rigidbody2D.velocity.magnitude);
         }
 
         if (transform.position.x <= -xBoundry)
         {
             Debug.Log("X'ten küçük");
             //transform.position = new Vector3(-xBoundry, transform.position.y, transform.position.z);
-            transform.position = new Vector3((transform.position.x + speed * Time.deltaTime), transform.position.y, transform.position.z);
+            //transform.position = new Vector3(-transform.position.x, transform.position.y, transform.position.z);
+            //rigidbody2D.AddForce(new Vector2(speed, 0));
+            rigidbody2D.velocity = -(rigidbody2D.velocity);
+            //Debug.Log(rigidbody2D.velocity.magnitude);
         }
 	}
 }
